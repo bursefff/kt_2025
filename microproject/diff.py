2962988b1172358289d824ee984c247a8d55e4e2
bursefff <bursefff@gmail.com>
2025-04-20 12:41:16 +0300

    micro1

---
+++ microproject/diff.py
@@ -0,0 +1,29 @@
+﻿import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+
+from diffractio import degrees, plt, um, np
+from diffractio.scalar_masks_XY import Scalar_mask_XY
+from diffractio.scalar_sources_XY import Scalar_source_XY
+
+num_pixels = 512
+
+length = 5000*um
+x0 = np.linspace(-length/2, length/2, num_pixels)
+y0 = np.linspace(-length/2, length/2, num_pixels)
+wavelength = 0.6238*um
+
+u1 = Scalar_source_XY(x=x0, y=y0, wavelength=wavelength)
+u1.gauss_beam(r0=(0*um, 0*um), w0=(1000*um, 1000*um), z0=0*um,
+    A=1, theta=0*degrees, phi=0*degrees)
+
+
+t1 = Scalar_mask_XY(x=x0, y=y0, wavelength=wavelength)
+t1.image(filename="fork1200.png", invert=False)
+
+u2 = u1 * t1
+
+u3 = u2.RS(z=300000*um, new_field=True)
+u3.draw(kind="intensity")
+plt.savefig('res1200.png')
---
+++ microproject/gen.py
@@ -0,0 +1,21 @@
+﻿import numpy as np
+import matplotlib.pyplot as plt
+dpi = 1200 #dots per inch
+size = 5000 #microns
+m = 4 #topological charge
+npixels = int(dpi *  size / 10000 /2.5)
+d = 100 #grating period
+x = np.linspace(-size/2, size/2, npixels)
+y = np.linspace(-size/2, size/2, npixels)
+t = [[0 for j in range(npixels)] for i in range(npixels)]
+for i in range(npixels):
+    for j in range(npixels):
+        ans = 0.5 * (1 + np.cos(2 * np.pi/d * y[j] - m * np.arctan2(y[j], x[i])))
+        t[i][j] = ans
+        # if ans < 0.5:
+        #     t[i][j] = 0
+        # else:
+        #     t[i][j] = 1
+plt.imshow(t, interpolation='none', cmap="Greys")
+plt.axis('off')
+plt.savefig('fork1200.png', bbox_inches='tight')
